f9f282befdf91e9cae3339632610ac4e3c2eb5c4
tingwei0518 <tingwei0518@gmail.com>
2025-05-14 21:32:40 +0800

    Create the user model

---
+++ prisma/migrations/20250514133126_create_user/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `gender` ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL,
+    `birthday` DATETIME(3) NOT NULL,
+    `occupation` ENUM('STUDENT', 'ENGINEER', 'TEACHER', 'UNEMPLOYED') NOT NULL,
+    `profileImage` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
